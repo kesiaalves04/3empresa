d65eed8677070bc64c08f642067d0b37f0b72233
kesia alves <kesia.alves@academico.ifpb.edu.br>
2026-05-20 12:03:39 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,28 +1,55 @@
+
 import streamlit as st
 
+# CONFIG
 st.set_page_config(page_title="Empresas Parceiras", layout="wide")
 
+# TÍTULO
 st.title("🌐 Empresas Parceiras")
 
+# COLUNAS
 col1, col2, col3 = st.columns(3)
 
-# SPACE X
+# =========================
+# SPACEX
+# =========================
 with col1:
-    st.image("espaçox.png", use_container_width=True)
+    st.image("spaçox.png")
     st.subheader("🚀 SpaceX")
-    st.write("Empresa aeroespacial criada por Elon Musk.")
+
+    st.write("""
+    Empresa aeroespacial criada por Elon Musk.
+    Atua no desenvolvimento de foguetes e viagens espaciais.
+    """)
+
     st.link_button("Acessar Site", "https://www.spacex.com/")
 
+# =========================
 # APPLE
+# =========================
 with col2:
-    st.image("iphone.png", use_container_width=True)
+    st.image("iphone.png")
     st.subheader("🍎 Apple")
-    st.write("Empresa mundialmente conhecida pela tecnologia.")
+
+    st.write("""
+    Empresa mundialmente conhecida pelos seus produtos tecnológicos,
+    como iPhone, iPad e MacBook.
+    """)
+
     st.link_button("Acessar Site", "https://www.apple.com/br/")
 
+# =========================
 # NETFLIX
+# =========================
 with col3:
-    st.image("netflix.png", use_container_width=True)
+    st.image("netflix.png")
     st.subheader("🎬 Netflix")
-    st.write("Plataforma de streaming de filmes e séries.")
+
+    st.write("""
+    Plataforma de streaming com filmes, séries e documentários
+    assistidos no mundo inteiro.
+    """)
+
     st.link_button("Acessar Site", "https://www.netflix.com/br/")
+
+
