2d577589c5e2391f5c3e39762928cd2156ca408d
kesia alves <kesia.alves@academico.ifpb.edu.br>
2026-05-20 11:59:37 -0300

    teste.py

--- teste.py
+++ teste.py
@@ -1,10 +1,6 @@
 import streamlit as st
-import os
 
 st.set_page_config(page_title="Empresas Parceiras", layout="wide")
-
-# pega caminho correto da pasta atual
-BASE_DIR = os.path.dirname(__file__)
 
 st.title("🌐 Empresas Parceiras")
 
@@ -12,21 +8,21 @@ col1, col2, col3 = st.columns(3)
 
 # SPACE X
 with col1:
-    st.image(os.path.join(BASE_DIR, "espacox.png"), use_container_width=True)
+    st.image("espaçox.png", use_container_width=True)
     st.subheader("🚀 SpaceX")
     st.write("Empresa aeroespacial criada por Elon Musk.")
     st.link_button("Acessar Site", "https://www.spacex.com/")
 
 # APPLE
 with col2:
-    st.image(os.path.join(BASE_DIR, "iphone.png"), use_container_width=True)
+    st.image("iphone.png", use_container_width=True)
     st.subheader("🍎 Apple")
-    st.write("Empresa mundial de tecnologia.")
+    st.write("Empresa mundialmente conhecida pela tecnologia.")
     st.link_button("Acessar Site", "https://www.apple.com/br/")
 
 # NETFLIX
 with col3:
-    st.image(os.path.join(BASE_DIR, "netflix.png"), use_container_width=True)
+    st.image("netflix.png", use_container_width=True)
     st.subheader("🎬 Netflix")
     st.write("Plataforma de streaming de filmes e séries.")
     st.link_button("Acessar Site", "https://www.netflix.com/br/")
